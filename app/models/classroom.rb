f5c37e2261c2bf6ea97a99766d01bb61759b46a2
Joey <joey@ubuntu.(none)>
2011-11-10 02:01:48 -0600

    move setup functions to Classroom

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -4,4 +4,18 @@ class Classroom < ActiveRecord::Base
 
   validates_presence_of :name
 
+  def self.setup_data
+    Teacher.create(name: 'Teacher1')
+    Teacher.new.save(validate: false)
+
+    Classroom.create(name: 'Classroom1')
+
+  end
+
+  def self.add_invalid_teacher_to_classroom
+    c = Classroom.first
+    t = Teacher.last
+    puts "Is teacher valid? #{t.valid?}"
+    c.teacher_ids = t.id
+  end
 end
